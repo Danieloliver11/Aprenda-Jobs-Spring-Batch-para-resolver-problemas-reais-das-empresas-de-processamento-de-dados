c1e74910a8fbdf565b9afe86fef5af1db63d7303
Danieloliver11 <dmoliveira@apoioprodesp.sp.gov.br>
2025-02-18 16:00:44 -0300

    primeiro job

---
+++ job-spring-batch/src/main/java/com/udemy/spring_batch/JobSpringBatchApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.spring_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JobSpringBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JobSpringBatchApplication.class, args);
+	}
+
+}
---
+++ job-spring-batch/src/test/java/com/udemy/spring_batch/JobSpringBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.spring_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JobSpringBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
